06f95d0885cf6e6b0796332cb56fcf3a272fa320
ceciliahong <cecilia.x.hong@ibm.com>
2017-04-06 17:01:23 -0300

    Trying to fix the detecting imageview bounds after rotation

--- RotateGestureDetector/app/src/main/java/com/example/xiaotinghong/rotategesturedetector/MainActivity.java
+++ RotateGestureDetector/app/src/main/java/com/example/xiaotinghong/rotategesturedetector/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
@@ -87,6 +88,18 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return null;
+    }
+
+    private boolean checkIfContainsInView(View view, float x, float y) {
+        int[] screenPosition = new int[2];
+        view.getLocationOnScreen(screenPosition);
+
+        int topLeftX = screenPosition[0];
+        int topLeftY = screenPosition[1];
+        int bottomRightX = screenPosition[0] + view.getWidth();
+        int bottomRightY = screenPosition[0] + view.getHeight();
+
+        return x >= topLeftX && x <= bottomRightX && y >= topLeftY && y <= bottomRightY;
     }
 
     private class MoveListener extends RotateGestureDetector.SimpleOnRotateGestureListener {
